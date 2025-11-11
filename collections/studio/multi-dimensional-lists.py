715c50d17e8f11af48c14ed00eea33b3f5d933c1
cwright8898 <cwright8898@gmail.com>
2025-11-11 16:48:47 -0600

    stuido commit

--- collections/studio/multi-dimensional-lists.py
+++ collections/studio/multi-dimensional-lists.py
@@ -1,3 +1,6 @@
+from operator import index
+
+
 food = "water bottles,meal packs,snacks,chocolate"
 equipment = "space suits,jet packs,tool belts,thermal detonators"
 pets = "parrots,cats,moose,alien eggs"
@@ -16,11 +19,21 @@ print(cargo_hold)
 
 # c) Query the user to select a cabinet (0 - 3) in the cargo_hold.
 
-cabinet_index = int(input("select cabinet number: "))
+cabinet_index = int(input("select cabinet number(0-3): "))
 
 
 # d) Use bracket notation and format to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if 0 <= 
+if cabinet_index <= 0 or cabinet_index > 3:
+    print("Error: select valid cabinet number")
+else:
+    print(f"the contents of cabinet, {cabinet_index}: {cargo_hold[cabinet_index]}")
 
 
-# e) Modify the code to query the user for BOTH a cabinet in cargo_hold AND a particular item. Use the in method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+# e) Modify the code to query the user for BOTH a cabinet in cargo_hold AND a particular item. Use the in method to check 
+# if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+item = input('Enter an item check: ')
+if item in cargo_hold[cabinet_index]:
+    print(f"cabinet {cabinet_index} does contain {item}")
+else:
+    print(f"cabrinet {cabinet_index} does not contain {item}")
